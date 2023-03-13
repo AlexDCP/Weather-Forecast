4e0ee235f246620ebed29c04eeca86d6ee7b33df
AlexDCP <alexdcmarriot@gmail.com>
2023-03-13 13:14:26 -0700

    cleaned up html and got javascript working. Local storage next!

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,8 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
 var apiKey = "aa8193195e53ae687af6138a4eac0137";
 var searchButton = document.getElementById('startsearch');
-
-// var searchInput = "miami";
-
-// in place of miami, you would want to capture your search input's value
-
-// Write your HARDCODED queryURL
-// var apiKey = "aa8193195e53ae687af6138a4eac0137";
 
 
 // encase this in a function
@@ -35,18 +21,51 @@ function getApi() {
             var latitude = data[0].lat;
             var queryUrl2 = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + apiKey;
             fetch(queryUrl2)
-            .then(function (response){
-                return response.json();
-            })
-            .then(function(data){
-                console.log(data);
-                // for (let i = 0; i < data.list.length; i+7){
-                //     console.log(data.list[i]);
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    var iconCode = data.list[0].weather[0].icon
+                    var iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`
+                    var todayForecast = $(`
+                    <div class="card-body d-flex justify-content-center">
+                        <h4 class="today">${data.city.name}</h4>
+                        <img class="icon" src="${iconUrl}">
+                        <p class="card-text weather">Temp:${data.list[0].main.temp}F°</p>
+                        <p class="card-text humidity">Humidity:${data.list[0].main.humidity}%</p>
+                        <p class="card-text windspeed">Wind:${data.list[0].wind.speed}MPH</p>
+                    </div>
+                `)
+                    $('.todaysweather').append(todayForecast);
+                    console.log(data);
+                    var fiveDayArray = data.list.filter(day => day.dt_txt.includes('12:00:00'));
+                    console.log(fiveDayArray);
+                    for (var i = 0; i < fiveDayArray.length; i++) {
+                        var currentTime = new Date(fiveDayArray[i].dt_txt).toLocaleString().split(',')[0];
+                        console.log(currentTime);
+                        var iconCode = fiveDayArray[i].weather[0].icon
+                        var iconPic = `https://openweathermap.org/img/w/${iconCode}.png`
+                        console.log(fiveDayArray[i].weather)
 
-                // }
-            })
+
+                        var fiveDayCard = $(`
+                        <div class="card col-2 m-2 w-22 bg-primary">
+                            <div class="card-body">
+                            <h4 class="time">${currentTime}</h4>
+                            <img class="icon" src="${iconPic}">
+                            <p class="card-text weather">Temp:${fiveDayArray[i].main.temp}F°</p>
+                            <p class="card-text humidity">Humidity:${fiveDayArray[i].main.humidity}%</p>
+                            <p class="card-text windspeed">Wind:${fiveDayArray[i].wind.speed} MPH</p>
+                            </div>
+                        </div>
+                        
+                        `);
+                        $('.fivedaycontainer').append(fiveDayCard);
+                    }
+
+
+                })
         });
 };
 // function to run your fetch
 searchButton.addEventListener('click', getApi);
-    // function to display data
